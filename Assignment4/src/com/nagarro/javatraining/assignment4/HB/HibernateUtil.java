4868f9ac9a30d0e486e9548b24f90326ffe121f6
Shreyansh shrey <shreyansh.shrey1999@gmail.com>
2022-01-22 21:23:57 +0530

    Delete Assignment4 directory

--- Assignment4/src/com/nagarro/javatraining/assignment4/HB/HibernateUtil.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.javatraining.assignment4.HB;
-
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
-public class HibernateUtil {
-	
-	public static SessionFactory sf =  new Configuration().configure().buildSessionFactory(); 
-}
