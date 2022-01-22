baf9fbaf1a3a2e21ce5577653cd94628f6b4a841
Shreyansh shrey <shreyansh.shrey1999@gmail.com>
2022-01-22 21:23:32 +0530

    Delete Assignment9 directory

--- Assignment9/.svn/pristine/65/6579f909d39f737fca05a32e3301774f0a835688.svn-base
+++
@@ -1,8 +0,0 @@
-package com.nagarro.javatraining.assignment9.Dao;
-
-import com.nagarro.javatraining.assignment9.modelObjects.Hr;
-
-public interface HrDao {
-	boolean addHrUser(Hr hrEmp);
-	Hr getHrUser(String userid);
-}
--- Assignment9/src/com/nagarro/javatraining/assignment9/HB/HibernateUtil.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.javatraining.assignment9.HB;
-
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
-public class HibernateUtil {
-	
-	public static SessionFactory sf =  new Configuration().configure().buildSessionFactory(); 
-}
