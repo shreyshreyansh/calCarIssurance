803dd5bcc55b688ac348a7283e951426b85c0f1b
Shreyansh shrey <shreyansh.shrey1999@gmail.com>
2022-01-22 21:24:13 +0530

    Delete Assignment3 directory

--- Assignment3/src/org/nagarro/javatraining/assignment3/hibernateUtil/HibernateSF.java
+++
@@ -1,9 +0,0 @@
-package org.nagarro.javatraining.assignment3.hibernateUtil;
-
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
-public class HibernateSF {
-
-	public static SessionFactory sf = new Configuration().configure().buildSessionFactory();  
-}
